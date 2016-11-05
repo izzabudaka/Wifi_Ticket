20e7a272b0eec1695f221abd54dfaa2ef28936ec
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-05 13:03:52 +0100

    fixed additions

--- journey_processor.js
+++ journey_processor.js
@@ -1,3 +1,4 @@
+// [{'entry', time: '100', location: 1}, {'exit', time: '101', location: 1}, {'enter', time: '102', location: 2}]
 this.partition_journey = function(all_travels){
 	var result = []
 	var cur = []
@@ -10,10 +11,7 @@ this.partition_journey = function(all_travels){
 					&& all_travels[curIndex].location_id == current_item.location_id){
 				curIndex++;
 			}
-			// if the last mention is not an exit, visit location
-			if(all_travels[curIndex-1].status == 'enter'){
-				cur.push(current_item)
-			}
+			cur.push(current_item)
 		} else if(cur.length > 0 && all_travels[curIndex].status == 'enter' 
 					&& routeExists(all_travels[curIndex], cur[cur.length-1])){
 			var current_item = all_travels[curIndex]
@@ -23,20 +21,17 @@ this.partition_journey = function(all_travels){
 				curIndex++;
 			}
 
-			// if the last mention is not an exit, visit location
-			if(all_travels[curIndex-1].status == 'enter'){
-				var date1 = new Date(cur[cur.length-1].timestamp)
-				var date2 = new Date(current_item.timestamp)
+			var date1 = new Date(cur[cur.length-1].timestamp)
+			var date2 = new Date(current_item.timestamp)
 
-				var difference = date1.getTime() - date2.getTime()	
-				var minutesDifference = Math.floor(difference/1000/60);
+			var difference = date1.getTime() - date2.getTime()	
+			var minutesDifference = Math.floor(difference/1000/60);
 
-				if(minutesDifference > 30){
-					result.push([cur[0], cur[cur.length-1]])
-					cur = []
-				} else{
-					cur.push(current_item)
-				}
+			if(minutesDifference > 30){
+				result.push([cur[0], cur[cur.length-1]])
+				cur = []
+			} else{
+				cur.push(current_item)
 			}
 		}
 	}
